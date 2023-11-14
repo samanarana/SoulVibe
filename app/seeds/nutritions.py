593e2b1caec109cbff07cec58c2161a57ae65ebc
Saman Rana <saman.ranaaa@gmail.com>
2023-11-13 20:48:50 -0600

    some seed changes add and view fully functional. edit and delete not added

--- app/seeds/nutritions.py
+++ app/seeds/nutritions.py
@@ -1,14 +1,29 @@
 from app.models import db, Nutrition
-from datetime import date
+from datetime import date, timedelta
+import random
 
 def seed_nutritions():
-    # List of meal types
-    meal_types = ["Breakfast", "Brunch", "Lunch", "Snack", "Dinner", "Dessert"]
-
+    meal_types = ["breakfast", "lunch", "snack", "dinner", "dessert"]
     nutrition_data = []
-    for user_id in range(1, 7):
-        for meal in meal_types:
-            nutrition_data.append(Nutrition(userId=user_id, date=date.today(), meal_type=meal))
+
+    # Example specific dates for each user
+    user_dates = {
+        1: date(2023, 11, 10),
+        2: date(2023, 11, 18),
+        3: date(2023, 11, 24),
+        4: date(2023, 11, 30),
+        5: date(2023, 12, 4),
+        6: date(2023, 12, 10)
+    }
+
+    # Seed data for a 7-day range for each user
+    for user_id in user_dates:
+        for day in range(8):
+            nutrition_date = user_dates[user_id] + timedelta(days=day)
+            selected_meal_types = random.sample(meal_types, 3)
+
+            for meal in selected_meal_types:
+                nutrition_data.append(Nutrition(userId=user_id, date=nutrition_date, meal_type=meal))
 
     db.session.add_all(nutrition_data)
     db.session.commit()
--- react-app/src/components/NutritionPage/index.js
+++ react-app/src/components/NutritionPage/index.js
@@ -2,11 +2,32 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchNutritionsThunk } from '../../store/nutrition';
 import AddMealForm from './AddMealForm';
+import './Nutrition.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+import breakfastIcon from './Icons/breakfast.png';
+import lunchIcon from './Icons/lunch.png';
+import dinnerIcon from './Icons/dinner.png';
+import snackIcon from './Icons/snack.png';
+import dessert2Icon from './Icons/dessert2.png';
 
 function NutritionPage() {
+
+  function getLocalDate() {
+    const now = new Date();
+    now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
+    return now.toISOString().split('T')[0];
+  }
+
   const dispatch = useDispatch();
   const nutritions = useSelector((state) => state.nutrition.nutritions);
-  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
+  const [selectedDate, setSelectedDate] = useState(getLocalDate());
+  const [selectedMeal, setSelectedMeal] = useState(null);
+  const [activeCard, setActiveCard] = useState(null);
+  const [selectedCardId, setSelectedCardId] = useState(null);
+
 
   useEffect(() => {
     dispatch(fetchNutritionsThunk());
@@ -22,34 +43,87 @@ function NutritionPage() {
     setSelectedDate(currentDate.toISOString().split('T')[0]);
   };
 
+  const handleCardClick = (nutritionId) => {
+    const mealDetails = nutritions[nutritionId];
+    setSelectedMeal(mealDetails);
+    setActiveCard(nutritionId);
+    setSelectedCardId(nutritionId);
+  };
+
+  const getMealIcon = (meal_type) => {
+    switch(meal_type) {
+      case 'breakfast': return breakfastIcon;
+      case 'lunch': return lunchIcon;
+      case 'dinner': return dinnerIcon;
+      case 'snack': return snackIcon;
+      case 'dessert': return dessert2Icon;
+      default: return null;
+    }
+  };
+
   return (
     <div className="nutrition-page-container">
 
-      <div className="date-cards-container">
+      <div className="left-side-container">
+        <div className="date-cards-container">
+
           <div className="date-navigator">
-            <button onClick={() => handleDateChange(-1)}>&lt;</button>
+            <button onClick={() => handleDateChange(-1)} style={{ border: 'none' }}>
+              <FontAwesomeIcon icon={faChevronLeft} />
+            </button>
             <input
               type="date"
               value={selectedDate}
               onChange={(e) => setSelectedDate(e.target.value)}
             />
-            <button onClick={() => handleDateChange(1)}>&gt;</button>
+            <button onClick={() => handleDateChange(1)} style={{ border: 'none' }}>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </button>
           </div>
-          <div className="cards-container">
-            {nutritionsForSelectedDate.map((nutrition) => (
-              <div key={nutrition.id} className="nutrition-card">
-                {/* Your card content here */}
-                <p>{nutrition.meal_type}</p>
-                {/* ... other nutrition details */}
+
+          <div className="cards-container-hello">
+            {nutritionsForSelectedDate.length > 0 ? (
+              nutritionsForSelectedDate.map((nutrition) => (
+                <div
+                  key={nutrition.id}
+                  className={`nutrition-card ${selectedCardId === nutrition.id ? 'selected-card' : ''}`}
+                  onClick={() => handleCardClick(nutrition.id)}
+                  style={{ zIndex: activeCard === nutrition.id ? 10 : 1 }}
+                >
+                  <img className="nutrition-card-icon" src={getMealIcon(nutrition.meal_type)} alt={nutrition.meal_type} />
+                </div>
+              ))
+            ) : (
+              <div className="nutrition-card">
+                <p>No meals today :(</p>
               </div>
-            ))}
+            )}
           </div>
+
+          {selectedMeal && (
+            <div className="meal-details-container">
+              <div className="meal-detail">
+                <p>My {selectedMeal.meal_type} !</p>
+                <ul>
+                  {selectedMeal.nutrition_details.map(detail => (
+                    <li key={detail.id}>
+                      {detail.description} - {detail.amount}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          )}
+
+
+        </div>
       </div>
+
 
         <AddMealForm />
 
     </div>
   );
-};
+}
 
 export default NutritionPage;
